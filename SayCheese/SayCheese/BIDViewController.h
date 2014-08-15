d70f94408dd73ec6e0c57fd61f80e6dc91d48eb7
gkopevski <gkopevski@gmail.com>
2014-08-15 18:48:26 +0200

    image is saved to camera roll after smile detection

--- SayCheese/SayCheese/BIDViewController.h
+++ SayCheese/SayCheese/BIDViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+@class CIDetector;
 
 @interface BIDViewController : UIViewController
+<UIGestureRecognizerDelegate,
+AVCaptureVideoDataOutputSampleBufferDelegate>
+{
+    IBOutlet UIView *previewView;
+	IBOutlet UISegmentedControl *camerasControl;
+	AVCaptureVideoPreviewLayer *previewLayer;
+	AVCaptureVideoDataOutput *videoDataOutput;
+	dispatch_queue_t videoDataOutputQueue;
+	AVCaptureStillImageOutput *stillImageOutput;
+	UIView *flashView;
+	UIImage *square;
+	BOOL isUsingFrontFacingCamera;
+	CIDetector *faceDetector;
+    CIDetector *smileDetector;
+	CGFloat beginGestureScale;
+	CGFloat effectiveScale;
+}
 
+- (IBAction)switchCameras:(id)sender;
+- (IBAction)handlePinchGesture:(UIGestureRecognizer *)sender;
 @end
